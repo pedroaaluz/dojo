eef02ae76f15e453dffb146009a4ee26265c60e9
Rodrigo Bitu <rodrigo.moraes@enacom.com.br>
2022-05-05 12:42:48 -0300

    Feat: second test

--- index.test.js
+++ index.test.js
@@ -1,34 +1,51 @@
 const conta = {
   saldo: 0,
   nome: 'Bitu',
-  sacar: (valor) => { 
-    
-    if(this.saldo <= 0) return 'Tu ta pobre'
-    if (valor > this.saldo) return 'Tu não tem grana pra isso'
-    
-    this.saldo -= valor 
+  sacar: function (valor) {
+    if (this.saldo <= 0) return 'Tu ta pobre';
+    if (valor > this.saldo) return 'Tu não tem grana pra isso';
 
-    return this.saldo
+    this.saldo -= valor;
+
+    return this.saldo;
   },
-  depositar: (valor) => { 
-    this.saldo += valor 
+  depositar: function (valor) {
+    this.saldo += valor;
 
-    return this.saldo
+    return this.saldo;
   },
-  consultarSaldo: function(){
-      return this.saldo
-  }
-  
-}
-
+  consultarSaldo: function () {
+    return this.saldo;
+  },
+};
 
 describe('Test teste : ', () => {
-  
   it('Deveria retornar o saldo em 0', () => {
     const valorInicial = 0;
     const result = conta.consultarSaldo();
 
     expect(result).toBe(valorInicial);
-  })
+  });
 
-})
+  it('Deveria retornar o saldo apos deposito', () => {
+    const valorDepositado = 100;
+    const saldo = conta.consultarSaldo();
+    const result = conta.depositar(valorDepositado);
+
+    expect(result).toBe(saldo + valorDepositado);
+  });
+
+  it('Deveria retornar o saldo apos saque', () => {
+    const valorSacado = 50;
+    const saldo = conta.consultarSaldo();
+    const result = conta.sacar(valorSacado);
+
+    expect(result).toBe(saldo - valorSacado);
+  });
+
+  it('Deveria retornar o saldo final', () => {
+    const result = conta.consultarSaldo(valorDepositado);
+
+    expect(result).toBe(saldo + valorDepositado);
+  });
+});
