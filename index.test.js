8cd43e02f1e6ae1e17f92cd6337e16f42916c64a
jvkikuchi <jvkikuchi23@gmail.com>
2022-05-05 12:53:33 -0300

    feat: testes finalizados

--- index.test.js
+++ index.test.js
@@ -19,24 +19,40 @@ const conta = {
   },
 };
 
-describe('Test teste : ', () => {
+describe('Testando metodos da conta : ', () => {
+  const valorSacado = 50;
+  const valorDepositado = 100;
+  const valorInicial = 0;
+
   it('Deveria retornar o saldo em 0', () => {
-    const valorInicial = 0;
     const result = conta.consultarSaldo();
 
     expect(result).toBe(valorInicial);
   });
 
+  it('Deveria retornar tu ta pobre', () => {
+    const result = conta.sacar(valorSacado);
+    const mensagem = 'Tu ta pobre';
+
+    expect(result).toBe(mensagem);
+  });
+
   it('Deveria retornar o saldo apos deposito', () => {
-    const valorDepositado = 100;
+   
     const saldo = conta.consultarSaldo();
     const result = conta.depositar(valorDepositado);
 
     expect(result).toBe(saldo + valorDepositado);
   });
 
+  it('Deveria retornar tu não tem grana pra isso', () => {
+    const result = conta.sacar(150);
+    const mensagem = 'Tu não tem grana pra isso';
+
+    expect(result).toBe(mensagem);
+  });
+
   it('Deveria retornar o saldo apos saque', () => {
-    const valorSacado = 50;
     const saldo = conta.consultarSaldo();
     const result = conta.sacar(valorSacado);
 
@@ -44,8 +60,7 @@ describe('Test teste : ', () => {
   });
 
   it('Deveria retornar o saldo final', () => {
-    const result = conta.consultarSaldo(valorDepositado);
-
-    expect(result).toBe(saldo + valorDepositado);
+    const saldo = conta.consultarSaldo();
+    expect(saldo).toBe(valorDepositado - valorSacado);
   });
 });
